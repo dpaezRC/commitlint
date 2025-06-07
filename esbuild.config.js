3416f246cb591e3351549f1aafa814294c0a8dbe
DiegoIpaez <diegoipaez2000@gmail.com>
2025-06-07 01:44:32 -0300

    feat: added esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,14 @@
+const esbuild = require("esbuild");
+
+esbuild
+  .build({
+    entryPoints: ["index.js"],
+    bundle: true,
+    platform: "node",
+    target: "node18",
+    outfile: "dist/bundle.js",
+    minify: true,
+    sourcemap: false,
+    external: ["express"],
+  })
+  .catch(() => process.exit(1));
